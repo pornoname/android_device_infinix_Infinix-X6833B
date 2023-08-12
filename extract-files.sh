4dde64e9833a8664e13a92ccfdc35bfd3a6b2a02
pornoname <118466846+pornoname@users.noreply.github.com>
2023-08-13 00:52:59 +0700

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -15,9 +15,9 @@ VENDOR=infinix
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-ANDROID_ROOT="${MY_DIR}/../../.."
+ANDROID_ROOT="${MY_DIR}"
 
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/android_tools_extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
